1ce35be99e56cf8a0f16eab5df817d31f5a496fd
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-19 15:15:58 +0000

    fetch resource, filter to render selected resource

--- src/components/FullResourcePage.tsx
+++ src/components/FullResourcePage.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router";
 import { BaseURL } from "../utils/baseURL";
 import useFetchResources from "../utils/fetchResources";
 import {
@@ -8,17 +9,21 @@ import {
   IResource,
 } from "../utils/interfaces";
 
-export default function FullResourcePage({
-  selectedResource,
-}: FullResourcesPageProps): JSX.Element {
+export default function FullResourcePage(): JSX.Element {
   const [commentList, setCommentList] = useState<IComments[]>();
-  const resource = useFetchResources();
-  const getCommentsFromServer = async () => {
+  const resourceList = useFetchResources();
+  const { id } = useParams();
+  const resource = resourceList.find((resource) => {
+    if (id) {
+      return resource.resource_id === parseInt(id);
+    }
+    return false;
+  });
+  const getCommentsFromServer = useCallback(async () => {
     console.log("fetching comment list from api");
+
     try {
-      const response = await axios.get(
-        BaseURL + "comments/" + selectedResource?.resource_id
-      );
+      const response = await axios.get(BaseURL + "comments/" + id);
 
       setCommentList(response.data.rows);
       console.log("newly retreived comments", response.data.rows);
@@ -26,24 +31,23 @@ export default function FullResourcePage({
       console.error("you have an error with spots");
     }
     console.log("finished with getcommentsFromServer");
-  };
-  useEffect(() => {
-    if (selectedResource) {
-      getCommentsFromServer();
-    }
   }, []);
+
+  useEffect(() => {
+    getCommentsFromServer();
+  }, [getCommentsFromServer]);
   return (
     <>
-      {selectedResource && (
+      {resource && (
         <div>
           <div className="resourcesInformation">
             <h1>
-              {selectedResource.resource_name}: {selectedResource.author_name}
+              {resource.resource_name}: {resource.author_name}
             </h1>
-            <p>{selectedResource.resource_description}</p>
-            <a href={selectedResource.resource_link}>Resource Link</a>
-            <p>{selectedResource.resource_likes}👍</p>
-            <p>{selectedResource.resource_dislikes}👎</p>
+            <p>{resource.resource_description}</p>
+            <a href={resource.resource_link}>Resource Link</a>
+            <p>{resource.resource_likes}👍</p>
+            <p>{resource.resource_dislikes}👎</p>
           </div>
           <div className="commentContainer">
             {commentList?.map((comment) => {
