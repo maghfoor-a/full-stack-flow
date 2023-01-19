0d308ed8858d0b616d5d46150f192d612a1e0413
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-19 13:47:30 +0000

    add state to track seleceted resource

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router";
+import { IResource } from "../utils/interfaces";
 import AddResourcePage from "./AddResourcePage";
 import FullResourcePage from "./FullResourcePage";
 import HomePage from "./HomePage";
 
 export default function MainContent(): JSX.Element {
+  const [selectedResource, setSelectedResource] = useState<IResource>();
+  const handleResourceClick = (selectedResource: IResource) => {
+    console.log("Resource Clicked", selectedResource);
+    setSelectedResource(selectedResource);
+  };
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={<HomePage handleResourceClick={handleResourceClick} />}
+        />
         <Route path="/addresource" element={<AddResourcePage />} />
-        <Route path="/fullresource/:id" element={<FullResourcePage />} />
+        <Route
+          path="/fullresource/:id"
+          element={<FullResourcePage selectedResource={selectedResource} />}
+        />
       </Routes>
     </>
   );
