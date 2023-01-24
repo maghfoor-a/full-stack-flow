55821ab45c1c28db6f49306952e754b7ed024397
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-24 11:58:27 +0000

    added a function to updateResources

--- src/utils/fetchResources.ts
+++ src/utils/fetchResources.ts
@@ -1,16 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BackendURL } from "./backendURL";
-import { IResource } from "./interfaces";
+import { IResource, useFetchResourcesType } from "./interfaces";
 
-export default function useFetchResources() {
+export default function useFetchResources(): useFetchResourcesType {
   const [resources, setResouces] = useState<IResource[]>([]);
-  useEffect(() => {
+
+  const updateResources = () => {
+    console.log("Trying to update resources")
     try {
       axios.get(BackendURL + "resources").then((res) => setResouces(res.data));
     } catch (error) {
       window.alert("Failed to fetch resources");
     }
+  }
+  useEffect(() => {
+    updateResources();
   }, []);
-  return resources;
+  return {resources, updateResources};
 }
