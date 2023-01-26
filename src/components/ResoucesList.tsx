9c5facdbd6a69e454f5a57eb92252ba209382154
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-26 11:51:09 +0000

    added search bar

--- src/components/ResoucesList.tsx
+++ src/components/ResoucesList.tsx
@@ -4,6 +4,7 @@ import "./ResourcesList.css";
 import axios from "axios";
 import { BackendURL } from "../utils/backendURL";
 import useFetchResources from "../utils/useFetchResources";
+import { useState } from "react";
 
 function textSummary(text: string, length: number) {
   let returnText = "";
@@ -18,6 +19,7 @@ function textSummary(text: string, length: number) {
 
 export function ResourcesList(): JSX.Element {
   const { resources, updateResources } = useFetchResources();
+  const [searchValue, setSearchValue] = useState("");
 
   const handleLikeResource = async (id: number) => {
     await axios.patch(BackendURL + `resources/likes/${id}`);
@@ -25,44 +27,56 @@ export function ResourcesList(): JSX.Element {
   };
   return (
     <>
+      <div>
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        ></input>
+      </div>
       {resources.length ? (
         <div className="resource-container">
-          {resources.map((resource) => {
-            return (
-              <div className="resource-item" key={resource.resource_id}>
-                <div className="resourceSummary">
-                  <h3 className="title">{resource.resource_name}</h3>
-                  <div className="resource-info">
-                    <button
-                      onClick={() => handleLikeResource(resource.resource_id)}
-                    >
-                      {resource.resource_likes}👍
-                    </button>
-                    <button>{resource.resource_dislikes}👎</button>
-                    <p>
-                      {format(
-                        Date.parse(resource.resource_post_date.toString()),
-                        "dd/MM/yy"
-                      )}
-                    </p>
+          {resources
+            .filter((resource) =>
+              resource.resource_name
+                .toLocaleLowerCase()
+                .includes(searchValue.toLocaleLowerCase())
+            )
+            .map((resource) => {
+              return (
+                <div className="resource-item" key={resource.resource_id}>
+                  <div className="resourceSummary">
+                    <h3 className="title">{resource.resource_name}</h3>
+                    <div className="resource-info">
+                      <button
+                        onClick={() => handleLikeResource(resource.resource_id)}
+                      >
+                        {resource.resource_likes}👍
+                      </button>
+                      <button>{resource.resource_dislikes}👎</button>
+                      <p>
+                        {format(
+                          Date.parse(resource.resource_post_date.toString()),
+                          "dd/MM/yy"
+                        )}
+                      </p>
 
-                    <p className="authorName">{resource.author_name}</p>
-                    <p className="description">
-                      {textSummary(resource.resource_description, 50)}
-                    </p>
-                    <p>{resource.resource_tags}</p>
+                      <p className="authorName">{resource.author_name}</p>
+                      <p className="description">
+                        {textSummary(resource.resource_description, 50)}
+                      </p>
+                      <p>{resource.resource_tags}</p>
 
-                    <Link
-                      className="resourceButton"
-                      to={`/fullresource/${resource.resource_id}`}
-                    >
-                      See Full Resource
-                    </Link>
+                      <Link
+                        className="resourceButton"
+                        to={`/fullresource/${resource.resource_id}`}
+                      >
+                        See Full Resource
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       ) : (
         <h1>Loading Data...</h1>
