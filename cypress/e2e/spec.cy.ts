9109c758c7f5caa0acf247b584e7bd8cdb9afb39
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-31 11:54:57 +0000

    add tests to check guest cannot add a resource

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,7 +4,7 @@ import { frontendURL } from "../../src/utils/frontendURL";
 describe("Visit add resources page and submit a resource", () => {
   it("visits home page and routes to resources page", () => {
     cy.visit(frontendURL);
-
+    cy.get("select").select(1);
     cy.contains("Add Resource!").click();
 
     cy.url().should("include", "/addresource");
@@ -26,5 +26,44 @@ describe("Visit add resources page and submit a resource", () => {
     cy.intercept("POST", "/resources").as("resource submitted!");
     cy.get("form").submit();
   });
-  
+});
+describe("Guests cannot add resource", () => {
+  it("stops guest going to add resource page", () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit(frontendURL);
+    cy.get(".navbarAddresource").click();
+    cy.contains("Please sign in to add a resource");
+
+    /* ==== End Cypress Studio ==== */
+  });
+  it("Guests cannot submit a resource", () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit("localhost:3000/addResource");
+    cy.get(".resourceName").type("Cypress test intro");
+    cy.get(".resourceName").type("Cypress test intro");
+    cy.get(".resourceDescription").type("Documentation for cypress");
+    cy.get(".resourceDescription").type("Documentation for cypress");
+    cy.get(".resourceAuthorName").type("Cypress.io");
+    cy.get(".resourceAuthorName").type("Cypress.io");
+    cy.get(".resourceLink").type(
+      "https://docs.cypress.io/guides/getting-started/opening-the-app"
+    );
+    cy.get(".resourceLink").type(
+      "https://docs.cypress.io/guides/getting-started/opening-the-app"
+    );
+    cy.get(".resourceContentType").type("Documentation");
+    cy.get(".resourceContentType").type("Documentation");
+    cy.get(".resourceRecommendation").select(
+      "I recommend this resource after having used it!"
+    );
+    cy.get(".resourceReason").type("Really useful and clear documentation");
+    cy.get(".resourceReason").type("Really useful and clear documentation");
+    cy.get(".resourceTag-TS").check();
+    cy.get(".resourceTag-React").check();
+    cy.get(".resourceTag-APIs").check();
+    cy.get(".resourceTag-Node").check();
+    cy.get("button").click();
+    cy.contains("Sign in to submit resource");
+    /* ==== End Cypress Studio ==== */
+  });
 });
